01267c3ee42e4474758628c663741de5bc391ee0
Danil Stafeychuk <danil_stafeychuk_97@mail.ru>
2024-04-15 17:31:22 +0700

    Delete lesson 7 directory

--- lesson 7/pages/CartPage.py
+++
@@ -1,9 +0,0 @@
-from selenium.webdriver.common.by import By
-
-class CartPage:
-
-    def __init__(self, driver):
-        self._driver = driver
-
-    def click_checkout(self):
-        self._driver.find_element(By.CSS_SELECTOR, '#checkout').click()
